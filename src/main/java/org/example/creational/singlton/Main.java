449ced7439cd5c5ac34dbfbf739b118ca41c8327
denis <denispasha@gmail.com>
2023-04-29 22:44:17 +0300

    update

--- src/main/java/org/example/creational/singlton/Main.java
+++ src/main/java/org/example/creational/singlton/Main.java
@@ -5,13 +5,13 @@ import org.example.training.creational.singlton.CarSingleton;
 public class Main {
     public static void main(String[] args) {
         // singleton instance of a car
-        CarSingleton instance = CarSingleton.getInstance();
+        CarSingleton instance = CarSingleton.getInstance("Toyota","Yaris","100");
         System.out.println(instance.hashCode());
 
-        CarSingleton instance1 = CarSingleton.getInstance();
+        CarSingleton instance1 = CarSingleton.getInstance("Toyota","Yaris","100");
         System.out.println(instance1.hashCode());
 
-        CarSingleton instance2 = CarSingleton.getInstance();
+        CarSingleton instance2 = CarSingleton.getInstance("Toyota","Yaris","100");
         System.out.println(instance2.hashCode());
     }
 
