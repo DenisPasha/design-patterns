b62c33648151e0b20d4f1a4a695d18ba3ebb82ae
Denis Pasha <denispasha95@gmail.com>
2023-08-10 18:36:24 +0300

    update

--- src/main/java/org/example/behavioral/strategy/Main.java
+++ src/main/java/org/example/behavioral/strategy/Main.java
@@ -11,5 +11,7 @@ public class Main {
         int resultOfSubtraction = contextSub.doOperation(3, 2);
 
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
